239408ed91f5c9f12883640ea7a2052f73087a54
root <lferran@student.ethz.ch>
2016-01-26 12:40:13 +0000

    a

---
+++ res/defaultconf.py
@@ -0,0 +1,38 @@
+"""
+Default configuration file
+"""
+
+# Hostname of the Traffic Engineering Controller host in the network.
+TEC_hostname = 'c3'
+
+# Hostname of the Traffic Generator host in the network.
+TG_hostname = 'c2'
+
+#Port on which the JSON-aware thread of the TEC is listening
+TEC_JsonPort = 5000
+
+# Place where the topology information is being stored inside the vm
+# (and the hosts)
+DB_path = '/tmp/db.topo'
+
+# Path of the Traffic Generator package
+TG_path = '/root/tecontroller/trafficgenerator/'
+
+# Path to the Traffic Engineering controller package
+TEC_path = '/root/tecontroller/tecontroller/'
+
+# Default port for the json-daemons for the hosts in the network
+Hosts_JsonPort = 5000
+
+# Path to the file where the flows definition for the Traffic
+# Generator are stored
+FlowFile = TG_path + 'flowfile.csv'
+
+# Waiting time (in seconds) for hosts to check their IP
+InitialWaitingTime = 10 
+
+# Default port for which IPERF server is listening in the custom hosts
+Hosts_DefaultIperfPort = '5001'
+
+# Log folder for the hosts
+Hosts_LogFolder = TG_path + "/logs/"
