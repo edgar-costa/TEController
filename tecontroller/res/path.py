baf33931b470e65944feacca1582bbc7dbc2f4ac
root <lferran@student.ethz.ch>
2016-01-31 10:32:28 +0000

    implemented path class and ipnetpath class

--- tecontroller/res/path.py
+++ tecontroller/res/path.py
@@ -4,21 +4,51 @@ This module implements the Path object class
 
 import ipaddress as ip
 
-
 class Path(object):
-    """Implements a path object representing the between prefixes in a
-    network
+    """Implements the abstract path object representing the between nodes
+    in a (general) network.
 
     """
-    def __init__(self, src=None, dst=None, route=[], edges={}):
-        self.src=src
-        self.dst=dst
+    def __init__(self, route=[], edges={}):
+
         self.route = route #Ordered list of nodes in the path. Nodes
                            #can be prefixes or routers.
         self.index = 0
         self.edges = edges #Dictionary containing information about the
                            #edges of the path: {(x, y):{'bw':1, 'weight':4},
                            #                    (y, u):{...}
+        self.src = self._setSrc(route)
+        self.dst = self._setDst(route)
+
+
+    def __setitem__(self, key, value):
+        if key not in ['route','edges']:
+            raise KeyError
+        elif key == 'route':
+            self.route = value
+            self.src = self._setSrc(value)
+            self.dst = self._setDst(value)
+        else: #'edges'
+            self.edges = value
+            
+    def __getitem__(self, key):
+        if key not in ['src','dst','route','edges']:
+            raise KeyError
+        else:
+            return self.__getattribute__(key)
+
+    def _setSrc(self, route):
+        if route != []:
+            return route[0]
+        else:
+            return None
+
+
+    def _setDst(self, route):
+        if route != []:
+            return route[-1]
+        else:
+            return None
 
     def __eq__(self, other):
          return (isinstance(other, self.__class__) and self.__dict__
@@ -35,23 +65,40 @@ class Path(object):
         S = "(%s -> %s): "
         return S%(self.src, self.dst)+str(self.route)
 
+
     def coincidentPaths(self, other):
-        []
-    
-    def getRoute(self):
-        return self.route
-    
-    def setRoute(self, route):
-        self.route = route
-        if nodes:
-            self.src = route[0]
-            self.dst = route[-1]
+        """Checks if two paths coincede in some edge along their
+        routes. Returns true if they do.
+
+        """
+        result = [True for ((a,b), data) in self.iter_edges() if
+                   (a,b) in other.getEdges().keys() or (b,a) in
+                   other.getEdges().keys()]
+        return result != []
+
+
+    def getCoincidentEdges(self, other):
+        """Given two paths, returns the edge information of those edges
+        present in both paths. Returns empty dictionary otherwise.
+
+        """
+        result = {(a,b): data for ((a, b), data) in self.iter_edges() if
+                   (a,b) in other.getEdges().keys() or (b,a) in
+                   other.getEdges().keys()}
+        return result
+                
+
+    def getEdgeInfo(self, x, y):
+        """Given edge (x,y), searches (x,y) or (y,x) in path and returns edge
+        information.
+
+        """
+        a = [data for edge, data in self.iter_edges() if (x,y) == edge
+             or (y,x) == edge]
+        if a != []:
+            return a[0]
         else:
-            self.src = None
-            self.dst = None
-            
-    def getEdges(self):
-        return self.edges
+            return {}
 
     def iter_nodes(self):
         for index in range(len(self.route)):
@@ -61,6 +108,11 @@ class Path(object):
         for (edge, data) in self.edges.iteritems():
             yield (edge, data)
 
+
+class IPNetPath(Path):
+    def __init__(self, route=[], edges={}):
+        super(IPNetPath, self).__init__(route, edges)          
+            
     def getMinBw(self):
         """Returns the lowest bandwidth found on links of the path
         """
