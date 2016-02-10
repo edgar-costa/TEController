5a495cb9e54e6b4075e5d1b0ae12d9cb35c8bb1e
root <lferran@student.ethz.ch>
2016-02-10 09:57:09 +0000

    removed dbhandler class from here and moved to /res

--- tecontroller/loadbalancer/lbcontroller.py
+++ tecontroller/loadbalancer/lbcontroller.py
@@ -14,10 +14,9 @@ from fibbingnode.algorithms.southbound_interface import SouthboundManager
 from fibbingnode.misc.igp_graph import IGPGraph
 from fibbingnode.misc.mininetlib import get_logger
 
-from fibbingnode.misc.mininetlib.ipnet import TopologyDB
-from fibbingnode import CFG
-
 from tecontroller.res import defaultconf as dconf
+from tecontroller.res.dbhandler import DatbaseHandler
+
 from tecontroller.res.path import IPNetPath
 from tecontroller.res.flow import Flow
 from tecontroller.loadbalancer.jsonlistener import JsonListener
@@ -54,84 +53,6 @@ class MyGraphProvider(SouthboundManager):
     """
     def received_initial_graph(self):
         HAS_INITIAL_GRAPH.set()        
-
-class DatabaseHandler(object):
-    def __init__(self):
-        # Read the topology info from DB_Path
-        self.db = TopologyDB(db=dconf.DB_Path)
-
-    def _db_getNameFromIP(self, x):
-        """Returns the name of the host or the router given the ip of the
-        router or the ip of the router's interface towards that
-        subnet.
-        """
-        if x.find('/') == -1: # it means x is a router id
-            ip_router = ipaddress.ip_address(x)
-            name = [name for name, values in
-                    self.db.network.iteritems() if values['type'] ==
-                    'router' and
-                    ipaddress.ip_address(values['routerid']) ==
-                    ip_router][0]
-            return name
-        
-        elif 'C' not in x: # it means x is an interface ip and not the
-                           # weird C_0
-            ip_iface = ipaddress.ip_interface(x)
-            for name, values in self.db.network.iteritems():
-                if values['type'] != 'router':
-                    for key, val in values.iteritems():    
-                        if isinstance(val, dict):
-                            ip_iface2 = ipaddress.ip_interface(val['ip'])
-                            if ip_iface.network == ip_iface2.network:
-                                return name
-
-
-    def _db_getIPFromHostName(self, hostname):
-        """Given the hostname of the host/host subnet, it returns the ip address
-        of the interface in the hosts side. It is obtained from TopoDB
-
-        It can also be called with a router name i.e: 'r1'
-        """
-        values = self.db.network[hostname]
-        if values['type'] == 'router':
-            return ipaddress.ip_address(values['routerid']).compressed
-        else:
-            ip = [ipaddress.ip_interface(v['ip']) for v in
-                  values.values() if isinstance(v, dict)][0]
-            return ip.compressed
-
-        
-    def _db_getSubnetFromHostName(self, hostname):
-        """Given the hostname of a host (e.g 's1'), returns the subnet address
-        in which it is connected.
-        """
-        hostinfo = [values for name, values in
-                    self.db.network.iteritems() if name == hostname
-                    and values['type'] != 'router'][0]
-        
-        if hostinfo is not None:
-            for key, val in hostinfo.iteritems():
-                if isinstance(val, dict) and 'ip' in val.keys():
-                    rname = key
-                    return self.db.subnet(hostname, rname)
-        else:
-            raise TypeError("Routers can't")
-
-
-    def _db_getConnectedRouter(self, hostname):
-        """Get connected router information from hostname given its name.
-
-        """
-        hostinfo = [values for name, values in
-                    self.db.network.iteritems() if name == hostname
-                    and values['type'] != 'router'][0]
-        
-        if hostinfo is not None:
-            for key, val in hostinfo.iteritems():
-                if isinstance(val, dict) and 'ip' in val.keys():
-                    router_name = key
-                    router_id = self._db_getIPFromHostName(router_name)
-                    return router_name, router_id
 
                 
 class LBController(DatabaseHandler):
