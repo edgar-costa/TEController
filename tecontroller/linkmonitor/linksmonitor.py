b86cceb080df2e35678ff2aa7ac705c8629d9bb2
root <lferran@student.ethz.ch>
2016-02-10 13:11:45 +0000

    coding linksmonitor

--- tecontroller/linkmonitor/linksmonitor.py
+++ tecontroller/linkmonitor/linksmonitor.py
@@ -6,19 +6,25 @@ network and will log the corresponding data.
 
 from tecontroller.res.snmplib import SnmpCounters
 from tecontroller.res.dbhandler import DatabaseHandler
-
+import time
 
 class LinksMonitor(DatabaseHandler):
     """
     Implements the class.
     """
-    def __init__(self):
+    def __init__(self, interval):
         super(LinksMonitor, self).__init__()
+        self.links = self._db_getAllEdges()
+        self.interval = interval
+        #self.counters = self.startCounters()
+        
+    def startCounters(self):
+        routers = self._db_getRouters()
+        self.counters = {name:'' for name, rid in routers}
 
-
-
-
-
-
+        
 if __name__ == '__main__':
+    refreshInterval = 1
+    lm = LinksMonitor(refreshInterval)
     
+    import ipdb; ipdb.set_trace()
