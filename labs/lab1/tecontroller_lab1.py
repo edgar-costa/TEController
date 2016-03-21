1ace39e6cf131b9792a57ae361f6c5fe0e2df6da
ferran <lferran@student.ethz.ch>
2016-03-21 11:16:46 +0100

    implementing...

--- labs/lab1/tecontroller_lab1.py
+++ labs/lab1/tecontroller_lab1.py
@@ -1,14 +1,57 @@
-from tecontroller.loadbalancer.lbcontroller import LBController
+from tecontroller.loadbalancer.simplepathlb import SimplePathLB
 from fibbingnode.misc.mininetlib import get_logger
+
+import Queue
 
 log = get_logger()
 
-class TEControllerLab1(LBController):
+class TEControllerLab1(SimplePathLB):
 	def __init__(self):
 		# Call init method from LBController
 		super(TEControllerLab1, self).__init__()
 
-		# Now do extra stuff here (like spawning the link monitor)
+		# Start the links monitorer thread linked to the event queue
+		lmt = LinksMonitorerThread(queue = self.eventQueue,
+							 	   network_graph = self.network_graph)
+		lmt.start()
+
+		# Graph that will hold the link available capacities
+		self.cg = self._createCapacitiesGraph()
+
+	def run(self):
+		"""Main loop that deals with new incoming events
+        """
+        while not self.isStopped():
+            # Get event from the queue (blocking)
+            event = self.eventQueue.get()
+            log.info(lineend)
+            t = time.strftime("%H:%M:%S", time.gmtime())
+            log.info("%s - run(): NEW event in the queue\n"%t)
+            log.info("\t* Type: %s\n"%event['type'])
+            
+            if event['type'] == 'newFlowStarted':
+                # Fetch flow from queue
+                flow = event['data']
+                log.info("\t* Flow: %s\n"%self.toLogFlowNames(flow))
+                
+                # Deal with new flow
+                self.dealWithNewFlow(flow)
+
+            elif event['type'] == 'newCapacitiesUpdate':
+             	# Update the capacities graph
+             	new_capacities_graph = event['data']
+             	self.updateCapacities(new_capacities_graph)
+   	            t = time.strftime("%H:%M:%S", time.gmtime())
+                log.info("%s - run(): network available capacities updated\n"%t)
+
+            else:
+                t = time.strftime("%H:%M:%S", time.gmtime())
+                log.info("%s - run(): UNKNOWN Event\n"%t)
+                log.info("\t* Event: "%str(event))
+
+    def updateCapacities(self, new_apacities_graph):
+    	# update self.cg wrt new_capacities_graph
+    	pass
 
 	def dealWithNewFlow(self, flow):
 		"""
@@ -26,7 +69,7 @@ class TEControllerLab1(LBController):
         src_network = src_iface.network
         dst_network = self.getCurrentOSPFPrefix(dst_iface.compressed)
 
-        # Get the string-type prefixes        
+        # Get the string-type prefixes
         src_prefix = src_network.compressed
         dst_prefix = dst_network.compressed
 
@@ -74,5 +117,18 @@ class TEControllerLab1(LBController):
     	        t = time.strftime("%H:%M:%S", time.gmtime())
         	    log.info("%s - dealWithNewFlow(): Flow will cause CONGESTION\n"%t)
 	        	
-	        	# We just allocate the flow to the currentPaths
-    	    	self.addAllocationEntry(dst_prefix, flow, currentPath)
+	        	# Call the subclassed method to properly 
+	        	# allocate flow to a congestion-free path
+            	self.flowAllocationAlgorithm(dst_prefix, flow, currentPath)
+
+
+
+    def _createCapacitiesGraph(self):
+		# Get copy of the network graph
+		ng_copy = self.network_graph.copy()
+		cg = self.network_graph.copy()
+		
+		for node in ng_copy.nodes_iter():
+			if not ng_copy.is_router(node):
+				cg.remove_node(node)
+		return cg
