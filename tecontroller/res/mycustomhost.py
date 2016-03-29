28284efc063641d42c4d7a435a10a86877d1bc7e
root <lferran@student.ethz.ch>
2016-03-29 20:51:43 +0000

    comment = True set for testing...

--- tecontroller/res/mycustomhost.py
+++ tecontroller/res/mycustomhost.py
@@ -29,7 +29,7 @@ log = get_logger()
 
 dconf.LBC_Path
 
-commented = False#True #
+commented = True #False#
 
 algo_to_file = {'lab1': dconf.Lab1_Path + 'tecontroller_lab1.py',
                 'lab2': dconf.Lab2_Path + 'tecontroller_lab2.py',
